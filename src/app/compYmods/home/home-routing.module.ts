7b65e184278c93782ff7935d5db78a5fc600cf81
GabrielOsOj <cuentaauxiliar2@hotmail.com>
2022-10-30 22:03:12 -0300

    Update home-routing.module.ts

--- src/app/compYmods/home/home-routing.module.ts
+++ src/app/compYmods/home/home-routing.module.ts
@@ -1,6 +1,3 @@
-import { ProyectosEditModalComponent } from './../compCompartidos/plant-modales/proyectos-edit-modal/proyectos-edit-modal.component';
-import { ProyectoTmpComponent } from './../compCompartidos/plant-proyecto/proyecto-tmp.component';
-import { PlantTarjetaComponent } from './../compCompartidos/plant-tarjeta/plant-tarjeta.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pagina/home.component';
